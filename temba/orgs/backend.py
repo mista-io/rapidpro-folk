b8b84fde37362f0e85bebd7979b590bfffe93b05
Bwanakweli Alex <bwanakweli4ever@gmail.com>
2024-02-20 10:07:31 +0200

    flowartisan auth fix

--- temba/orgs/backend.py
+++ temba/orgs/backend.py
@@ -57,79 +57,91 @@ class AuthenticationBackend(ModelBackend):
                 # Authentication was successful
                 access_token = response.json().get('access_token')
 
+
                 if access_token:
                     payload = decode_jwt_token(access_token)
                     if payload:
-                        check_and_update_subscription_status(payload)
+                       check_and_update_subscription_status(payload)
 
-                        account = payload.get('account')
-                        if account:
-                            plan = account.get('plan')
-                            if plan:
-                                options = plan.get('options')
-                                if options:
-                                    try:
-                                        json_data = json.loads(options)
-                                        flowartisan_access = json_data.get('flowartisan_access')
-                                        print(flowartisan_access)
-                                    except json.JSONDecodeError:
-                                        print("Error decoding JSON data in options.")
-                                else:
-                                    print("'options' not found in payload.")
-                            else:
-                                print("'plan' not found in payload.")
-                        else:
-                            print("'account' not found in payload.")
-                    else:
-                        print("Payload not found.")
+                    
+                    account = payload.get('account')
+                    if account:
+                        plan = account.get('plan')
 
-                    # Ensure necessary keys are present before accessing
-                    if payload and 'account' in payload and 'plan' in payload['account'] and 'options' in payload['account']['plan']:
-                        if 'flowartisan_access' not in payload['account']['plan']['options'] or payload['account']['plan']['options']['flowartisan_access'] == "no":
-                            return None  # Authentication failed
+                   
+                    # Ensure that options is a dictionary before using .get()
+                    options = payload['account']['plan']['options']
+                   
+                    json_data = json.loads(options)
 
-                        # Access user data from payload and create the user
-                        email = payload['account']['email']
-                        try:
-                            user = User.objects.get(username__iexact=email)
-                        except User.DoesNotExist:
-                            # Create user
-                            user = User.objects.create_user(
-                                username=email,
-                                email=email,
-                                first_name=payload['account']['firstname'],
-                                last_name=payload['account']['lastname'],
-                                password=None  # Password is handled by the authentication service
-                            )
+                   # get flowartisan_access from the options
+                    flowartisan_access = json_data.get('flowartisan_access')
+                    print(flowartisan_access)
+                   
+                   
+                    try:
+                        if payload is None or 'account' not in payload  or 'flowartisan_access' not in payload['account']['plan']['options'] or flowartisan_access == "no":
+                        # Your code here
+                            return None  # Authentication failed, return None instead of raising an exceptio
+                        
+                    except KeyError:
+                       print("Either 'account', 'plan', or 'options' key is missing in the payload")
+                    
 
-                            # Create organization and associate user with it
-                            org = Org.objects.create(name=payload['account']['organization'], timezone=settings.USER_TIME_ZONE, language=settings.DEFAULT_LANGUAGE, flow_languages='{eng}')
-                            org.add_user(user, OrgRole.ADMINISTRATOR)
+                    email = payload['account']['email']
+                    try:
+                        user = User.objects.get(username__iexact=email)
+                    except User.DoesNotExist:
+                        # create account from API
+                        logger.info("User does not exist, registering one")
 
-                            # Additional tasks
-                            analytics.identify(user, brand=request.branding, org=org)
-                            analytics.track(user, "temba.org_signup", properties=dict(org=org.name))                     
-                            switch_to_org(request, org)
-                            org.initialize(sample_flows=True)
+                        first_name = payload['account']['firstname']
+                        last_name = payload['account']['lastname']
+                        organization = payload['account']['organization']
 
-                            login(request, user)
-                            self.get_success_url()
+                        # Instead of creating user directly, use create_user to handle password hashing
+                        user = User.objects.create_user(
+                            username=email,
+                            email=payload['account']['email'],
+                            first_name=first_name,
+                            last_name=last_name,
+                            password=None  # Password is handled by the authentication service
+                        )
 
-                        return user
-                    else:
-                        print("Necessary keys are missing in the payload.")
-                        return None
+                        # create the Organization
+                        anonymous = User.objects.get(pk=1)  # the default anonymous user
+                        org_data = dict(name=organization, created_by=anonymous, modified_by=anonymous, 
+                                        timezone=settings.USER_TIME_ZONE, language=settings.DEFAULT_LANGUAGE,flow_languages='{eng}')
+
+                        org = Org.objects.create(**org_data)
+                       
+                        
+
+                        org.add_user(user, OrgRole.ADMINISTRATOR)
+                        logger.info("New user added to an organization")
+                        logger.info(org)
+
+                        # Additional tasks specific to your application
+                        #self.object = org  # Assuming self.object is used elsewhere in your code
+                        analytics.identify(user, brand=request.branding, org=org)
+                        analytics.track(user, "temba.org_signup", properties=dict(org=org.name))                     
+                        switch_to_org(request, org)
+                        org.initialize(sample_flows=True)
+
+                        login(request, user)
+                        self.get_success_url()
+
+                    return user
                 else:
-                    print("Access token not found in the response.")
                     return None
-            else:
-                print("Authentication failed with status code:", response.status_code)
-                return None
-        except Exception as e:
-            print("An error occurred during authentication:", str(e))
-            return None
+        except User.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a non-existing user (#20760).
+            User().set_password(password)
 
-        
+    # The rest of the AuthenticationBackend remains unchanged...
+
+       
 
     def get_user(self, user_id):
         try:
